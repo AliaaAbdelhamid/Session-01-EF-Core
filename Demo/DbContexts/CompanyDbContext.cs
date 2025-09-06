4e6927fa7409975cf9c4dc36fe65040b3d75e071
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2025-09-06 20:51:19 +0300

    DbContext

---
+++ Demo/DbContexts/CompanyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DbContexts
+{
+	internal class CompanyDbContext : DbContext
+	{
+		public CompanyDbContext():base()
+		{
+			
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server = .; Database = CompanyG06 ; Trusted_Connection = true");
+		}
+		
+	}
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Demo
+﻿using Demo.DbContexts;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        }
+
+            using CompanyDbContext dbContext = new CompanyDbContext(); // C# 8.0 syntax sugar
+
+		}
     }
 }
