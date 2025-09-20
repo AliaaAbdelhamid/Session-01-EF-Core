de0763bca20d8337191584d25450b825dd2f5597
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2025-09-20 18:51:15 +0300

    Session 04

--- DemoInhertiance/Program.cs
+++ DemoInhertiance/Program.cs
@@ -9,6 +9,8 @@ namespace DemoInheritance
 		static void Main(string[] args)
 		{
 			using MyCompanyDbContext dbContext = new MyCompanyDbContext();
+	
+			#region Session 04
 			//FulltimeEmployee fulltimeEmployee = new FulltimeEmployee()
 			//{
 			//	Name = "Omar",
@@ -39,19 +41,19 @@ namespace DemoInheritance
 			//dbContext.Add(employee);
 			//dbContext.SaveChanges();
 
-			var Employees = dbContext.Employees;
-			if (Employees is not null)
-			{
-				foreach (var employee in Employees)
-				{
-					Console.WriteLine($"{employee.Id} -- {employee.Name} -- {employee.Address} ");
-				}
-				//Console.WriteLine("===========================");
-				//foreach (var employee in Employees.OfType<FulltimeEmployee>())
-				//{
-				//	Console.WriteLine($"{employee.Id} -- {employee.Name} -- {employee.Address} -- {employee.Salary}");
-				//}
-			}
+			//var Employees = dbContext.Employees;
+			//if (Employees is not null)
+			//{
+			//	foreach (var employee in Employees)
+			//	{
+			//		Console.WriteLine($"{employee.Id} -- {employee.Name} -- {employee.Address} ");
+			//	}
+			//	//Console.WriteLine("===========================");
+			//	//foreach (var employee in Employees.OfType<FulltimeEmployee>())
+			//	//{
+			//	//	Console.WriteLine($"{employee.Id} -- {employee.Name} -- {employee.Address} -- {employee.Salary}");
+			//	//}
+			//}
 
 
 			//Console.WriteLine("=========================");
@@ -66,7 +68,9 @@ namespace DemoInheritance
 			//{
 			//		Console.WriteLine($"{PTEmployee.Id} -- {PTEmployee.Name} -- {PTEmployee.Address} -- {PTEmployee.HourRate}");
 
-			//}
+			//} 
+			#endregion
+
 		}
 	}
 }
